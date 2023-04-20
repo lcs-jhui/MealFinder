1c6b61edbe458fe0e7ebaf06c062db7aeb8e0b76
Justin Hui <jhui@lcs.on.ca>
2023-04-20 11:41:47 -0400

    Initial Commit

---
+++ MealFinder/MealFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealFinderApp.swift
+//  MealFinder
+//
+//  Created by Justin Hui on 20/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct MealFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
