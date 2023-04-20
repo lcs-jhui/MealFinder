1ac3f8fa4393152d30dff8dd1c75d2dc65a201bf
Justin Hui <jhui@lcs.on.ca>
2023-04-20 11:46:11 -0400

    Deleted content view and added meal view

---
+++ MealFinder/Model/Meal.swift
@@ -0,0 +1,8 @@
+//
+//  Meal.swift
+//  MealFinder
+//
+//  Created by Justin Hui on 20/4/2023.
+//
+
+import Foundation
---
+++ MealFinder/Views/MealView.swift
@@ -0,0 +1,28 @@
+//
+//  MealView.swift
+//  MealFinder
+//
+//  Created by Justin Hui on 20/4/2023.
+//
+
+import SwiftUI
+
+struct MealView: View {
+    
+    var body: some View {
+        
+        NavigationView{
+            
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            
+            
+                .navigationTitle("Meal Finder")
+        }
+    }
+}
+
+struct MealView_Previews: PreviewProvider {
+    static var previews: some View {
+        MealView()
+    }
+}
