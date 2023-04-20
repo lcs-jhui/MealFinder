0ec019ccc1eed347640c9d1ac37012b277fdf400
Justin Hui <jhui@lcs.on.ca>
2023-04-20 16:36:26 -0400

    Added a search bar

--- MealFinder/Views/SearchView.swift
+++ MealFinder/Views/SearchView.swift
@@ -13,35 +13,44 @@ struct SearchView: View {
     
     //Holds the list of meals returned by our search
     @State var foundMeals: [Meal] = []
+    
+    //Holds the song name the user is searching for
+    @State var searchText = ""
+    
+    //Debugging if it succeeded or failed
     @State var taskDone: Bool = false
     var body: some View {
         
-        //Show the list of meals that match the search term
-        ZStack {
-            if taskDone {
-                List(foundMeals, id: \.idMeal) { currentMeal in
-                    
-                    HStack{
+        NavigationView{
+            
+            //Show the list of meals that match the search term
+            ZStack {
+                if taskDone {
+                    List(foundMeals, id: \.idMeal) { currentMeal in
                         
-                        AsyncImage(url: URL(string: currentMeal.strMealThumb),scale: 2)
-                            .scaledToFit()
-                            .frame(width: 50, height: 50)
-                            .clipped()
-                        
-                        Text(currentMeal.strMeal)
-                            .bold()
+                        HStack{
+                            
+                            AsyncImage(url: URL(string: currentMeal.strMealThumb),scale: 2)
+                                .scaledToFit()
+                                .frame(width: 50, height: 50)
+                                .clipped()
+                            
+                            Text(currentMeal.strMeal)
+                                .bold()
+                            
+                        }
                         
                     }
-                    
+                } else {
+                    ProgressView()
                 }
-            } else {
-                ProgressView()
             }
-        }
-        .task{
-            //when the view appears, fetch search results for beef
-            foundMeals = await NetworkService.fetch(resultsFor: "chicken")
-            taskDone.toggle()
+            .searchable(text: $searchText)
+            .task{
+                //when the view appears, fetch search results for beef
+                foundMeals = await NetworkService.fetch(resultsFor: "chicken")
+                taskDone.toggle()
+            }
         }
         
     }
