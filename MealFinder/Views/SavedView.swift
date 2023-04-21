8d4ee8d5cfa663f614941bde83715ad7092d67ef
Justin Hui <jhui@lcs.on.ca>
2023-04-21 09:41:09 -0400

    write the query that will access the database to see the saved meals

--- MealFinder/Views/SavedView.swift
+++ MealFinder/Views/SavedView.swift
@@ -5,9 +5,19 @@
 //  Created by Justin Hui on 20/4/2023.
 //
 
+import Blackbird
 import SwiftUI
 
 struct SavedView: View {
+    
+    //MARK: Stored Properties
+    
+    //The list of saved meals
+    @BlackbirdLiveModels ({ db in
+        try await Meal.read(from: db)
+    }) var savedMeals
+    
+    //MARK: Computed Properties
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
@@ -16,5 +26,6 @@ struct SavedView: View {
 struct SavedView_Previews: PreviewProvider {
     static var previews: some View {
         SavedView()
+            .environment(\.blackbirdDatabase, AppDatabase.instance)
     }
 }
