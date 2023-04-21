308854bd9261b9db980138355b6e503a8dea6e01
Justin Hui <jhui@lcs.on.ca>
2023-04-21 09:35:12 -0400

    Added a view to see saved data

---
+++ MealFinder/Views/SavedView.swift
@@ -0,0 +1,20 @@
+//
+//  SavedView.swift
+//  MealFinder
+//
+//  Created by Justin Hui on 20/4/2023.
+//
+
+import SwiftUI
+
+struct SavedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SavedView_Previews: PreviewProvider {
+    static var previews: some View {
+        SavedView()
+    }
+}
